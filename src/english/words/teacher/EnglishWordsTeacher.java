a5cfac1c1f5e6760cc481b2360df20328bd8228c
Andrei_Matveev <matandpet@gmail.com>
2016-08-24 05:22:11 -0300

    today's first commit

---
+++ src/english/words/teacher/EnglishWordsTeacher.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package english.words.teacher;
+
+/**
+ *
+ * @author am
+ */
+public class EnglishWordsTeacher {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
